a0bd3d05040ba404d6dc045739c71ccf5fbaa829
Christine Tham <chris.tham@hellotham.com>
2020-12-31 17:19:52 +1100

    Fixed task

--- src/components/Task.js
+++ src/components/Task.js
@@ -8,16 +8,21 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
     <div className={`list-item ${state}`}>
       <label className="checkbox">
         <input
+          type="checkbox"
+          defaultChecked={state === 'TASK_ARCHIVED'}
+          disabled={true}
+          name="checked"
+        />
+        <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
+      </label>
+      <div className="title">
+        <input
           type="text"
           value={title}
           readOnly={true}
           placeholder="Input title"
           style={{ textOverflow: 'ellipsis' }}
         />
-        <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
-      </label>
-      <div className="title">
-        <input type="text" value={title} readOnly={true} placeholder="Input title" />
       </div>
 
       <div className="actions" onClick={event => event.stopPropagation()}>
