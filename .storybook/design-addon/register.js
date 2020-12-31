d265eec1bf3098417760f9e9643112aa6f7c7781
Christine Tham <chris.tham@hellotham.com>
2020-12-31 17:48:42 +1100

    Updated addon

--- .storybook/design-addon/register.js
+++ .storybook/design-addon/register.js
@@ -1,8 +1,8 @@
 //.storybook/design-addon/register.js
 
 import React, { Fragment } from 'react';
-import { AddonPanel } from '@storybook/components';
-import { useParameter, useStorybookState } from '@storybook/api';
+import { useParameter, useStorybookState, useAddonState } from '@storybook/api';
+import { AddonPanel, ActionBar } from '@storybook/components';
 import { addons, types } from '@storybook/addons';
 import { styled } from '@storybook/theming';
 
@@ -35,23 +35,37 @@ const Asset = ({ url }) => {
 };
 
 const Content = () => {
-  // story's parameter being retrieved here
-  const results = useParameter('assets', []);
-  // the id of story retrieved from Storybook global state
-  const { storyId } = useStorybookState();
-
-  if (results.length === 0) {
-    return null;
-  }
-
-  const url = getUrl(results[0]).replace('{id}', storyId);
-
-  return (
-    <Fragment>
-      <Asset url={url} />
-    </Fragment>
-  );
-};
+    // story's parameter being retrieved here
+    const results = useParameter('assets', []);
+    // addon state being persisted here
+    const [selected, setSelected] = useAddonState('my/design-addon', 0);
+    // the id of the story retrieved from Storybook global state
+    const { storyId } = useStorybookState();
+  
+    if (results.length === 0) {
+      return null;
+    }
+  
+    if (results.length && !results[selected]) {
+      setSelected(0);
+      return null;
+    }
+  
+    const url = getUrl(results[selected]).replace('{id}', storyId);
+    return (
+      <Fragment>
+        <Asset url={url} />
+        {results.length > 1 ? (
+          <ActionBar
+            actionItems={results.map((i, index) => ({
+              title: typeof i === 'string' ? `asset #${index + 1}` : i.name,
+              onClick: () => setSelected(index),
+            }))}
+          />
+        ) : null}
+      </Fragment>
+    );
+  };
 
 addons.register('my/design-addon', () => {
     addons.add('design-addon/panel', {
