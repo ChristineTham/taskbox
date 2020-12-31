784462f3f0af9f2e783535a9ffbaffa6f38f88eb
Christine Tham <chris.tham@hellotham.com>
2020-12-31 17:29:10 +1100

    Created design addon

---
+++ .storybook/design-addon/register.js
@@ -0,0 +1,17 @@
+//.storybook/design-addon/register.js
+
+import React from 'react';
+import { AddonPanel } from '@storybook/components';
+import { addons, types } from '@storybook/addons';
+
+addons.register('my/design-addon', () => {
+  addons.add('design-addon/panel', {
+    title: 'Assets',
+    type: types.PANEL,
+    render: ({ active, key }) => (
+      <AddonPanel active={active} key={key}>
+        implement
+      </AddonPanel>
+    ),
+  });
+});
