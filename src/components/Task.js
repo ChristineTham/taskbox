c583e1c75c5358dcee0f32506bab39b4924ad82a
Christine Tham <chris.tham@hellotham.com>
2020-12-31 17:11:37 +1100

    Add long title edge case

--- src/components/Task.js
+++ src/components/Task.js
@@ -8,10 +8,11 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
     <div className={`list-item ${state}`}>
       <label className="checkbox">
         <input
-          type="checkbox"
-          defaultChecked={state === 'TASK_ARCHIVED'}
-          disabled={true}
-          name="checked"
+          type="text"
+          value={title}
+          readOnly={true}
+          placeholder="Input title"
+          style={{ textOverflow: 'ellipsis' }}
         />
         <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
